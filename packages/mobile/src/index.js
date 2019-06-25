d3cf099ff8fbfd4db569848ea40ce9c711732590
Carlos Levir <carloslevirfs@gmail.com>
2019-06-25 14:46:28 -0300

    Build of Mobile App.

---
+++ packages/mobile/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
+
+import Routes from './routes';
+
+export default function App() {
+  return <Routes />;
+}
